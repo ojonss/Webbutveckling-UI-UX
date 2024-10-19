3630b371875c8df7feb238c566252d9a047c666c
oskar <oskar.jonsson@chasacademy.se>
2024-10-19 20:09:35 +0200

    update dynamicMap.js

--- Ovningar/week3/map/dynamicMap.js
+++ Ovningar/week3/map/dynamicMap.js
@@ -1,14 +1,21 @@
 function initializeMap() {
+    //10an betyder att vi använder den vanliga talbasen 0-9
     const width = parseInt(document.getElementById("gridWidth").value, 10);
     const height = parseInt(document.getElementById("gridHeight").value, 10);
 
-    // Create the full map
-    const fullMap = new MapPlacement(width, height);
+    if (width < 9 || height < 9) {
+        alert("Min mapsize is 9x9")
+    }
+    else {
+        // Create the full map
+        const fullMap = new MapPlacement(width, height);
 
-    const apa = document.querySelector('.generateMenu');
-    apa.classList.add('hidden');
+        const apa = document.querySelector('.generateMenu');
+        apa.classList.add('hidden');
 
 
-    // Game starts here
-    new Movement(fullMap);
+        // Game starts here
+        new Movement(fullMap);
+    }
+
 }
